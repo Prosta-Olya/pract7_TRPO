a577db1dab3ecf8964190e4594bd4aeaadab2988
Prosta-Olya <siverolya@gmail.com>
2025-10-22 11:40:01 +0500

    Add files via upload

---
+++ Pract7/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Patient
+{
+    public int Id { get; set; } = 0;
+    public string Name { get; set; } = "Имя";
+    public string LastName { get; set; } = "Фамилия";
+    public string MiddleName { get; set; } = "Отчество";
+    public DateTime Birthday { get; set; }
+    public DateTime LastAppointment { get; set; }
+    public int LastDoctor { get; set; } = 0;
+    public string Diagnosis { get; set; } = "Диагноз";
+    public string Recomendations { get; set; } = "Рекомендация";
+    public Patient()
+	{
+	}
+}
---
+++ Pract7/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class User
+{
+    public int Id { get; set; } = 0;
+    public string Name { get; set; } = "Имя";
+    public string LastName { get; set; } = "Фамилия";
+    public string Specialisation { get; set; } = "Специализация";
+    public string Password { get; set; } = "Пароль";
+    public User()
+	{
+        //Random random = new Random();
+
+	}
+}
